18d9b60832e6238e39bf285526ec6680350cf4af
Pengying Xu <xpy66swsry@gmail.com>
2025-02-22 20:52:07 +0800

    fix: redirect domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/colibrow.github.io/',
+  base: 'https://blog.maxxxu.com/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
